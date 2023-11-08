7bdace5aef4bb1c16b62c636a70ef97c0d099f81
yuanyongxin <yuanyongxin@kedacom.com>
2023-11-08 09:57:23 +0800

    修改调试日志

--- Source/RuntimeToolsSystem/Public/Tools/RuntimeCustomTool.cpp
+++ Source/RuntimeToolsSystem/Public/Tools/RuntimeCustomTool.cpp
@@ -29,6 +29,8 @@ URuntimeCustomTool::URuntimeCustomTool()
 
 void URuntimeCustomTool::Setup()
 {
+	UE_LOG(LogTemp, Log, TEXT("URuntimeCustomTool::Setup()"));
+
 	UMultiClickSequenceInputBehavior* MouseBehavior = NewObject<UMultiClickSequenceInputBehavior>();
 	MouseBehavior->Initialize(this);
 	MouseBehavior->Modifiers.RegisterModifier(1, FInputDeviceState::IsShiftKeyDown);
@@ -42,41 +44,41 @@ void URuntimeCustomTool::OnBeginSequencePreview(const FInputDeviceRay& ClickPos)
 
 bool URuntimeCustomTool::CanBeginClickSequence(const FInputDeviceRay& ClickPos)
 {
-	UE_LOG(LogTemp, Log, TEXT("URuntimeCustomTool::OnBeginSequencePreview()"));
+	UE_LOG(LogTemp, Log, TEXT("URuntimeCustomTool::CanBeginClickSequence()"));
 
 	return true;
 }
 
 void URuntimeCustomTool::OnBeginClickSequence(const FInputDeviceRay& ClickPos)
 {
-	UE_LOG(LogTemp, Log, TEXT("URuntimeCustomTool::OnBeginSequencePreview()"));
+	UE_LOG(LogTemp, Log, TEXT("URuntimeCustomTool::OnBeginClickSequence()"));
 }
 
 void URuntimeCustomTool::OnNextSequencePreview(const FInputDeviceRay& ClickPos)
 {
-	UE_LOG(LogTemp, Log, TEXT("URuntimeCustomTool::OnBeginSequencePreview()"));
+	UE_LOG(LogTemp, Log, TEXT("URuntimeCustomTool::OnNextSequencePreview()"));
 }
 
 bool URuntimeCustomTool::OnNextSequenceClick(const FInputDeviceRay& ClickPos)
 {
-	UE_LOG(LogTemp, Log, TEXT("URuntimeCustomTool::OnBeginSequencePreview()"));
+	UE_LOG(LogTemp, Log, TEXT("URuntimeCustomTool::OnNextSequenceClick()"));
 
 	return true;
 }
 
 void URuntimeCustomTool::OnTerminateClickSequence()
 {
-	UE_LOG(LogTemp, Log, TEXT("URuntimeCustomTool::OnBeginSequencePreview()"));
+	UE_LOG(LogTemp, Log, TEXT("URuntimeCustomTool::OnTerminateClickSequence()"));
 }
 
 bool URuntimeCustomTool::RequestAbortClickSequence()
 {
-	UE_LOG(LogTemp, Log, TEXT("URuntimeCustomTool::OnBeginSequencePreview()"));
+	UE_LOG(LogTemp, Log, TEXT("URuntimeCustomTool::RequestAbortClickSequence()"));
 
 	return true;
 }
 
 void URuntimeCustomTool::OnUpdateModifierState(int ModifierID, bool bIsOn)
 {
-	UE_LOG(LogTemp, Log, TEXT("URuntimeCustomTool::OnBeginSequencePreview()"));
+	UE_LOG(LogTemp, Log, TEXT("URuntimeCustomTool::OnUpdateModifierState()"));
 }
